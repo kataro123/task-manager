38028d8c47e570294f3f504019424ebf6a165895
Filipe de Araujo <filipearrudafaga97@gmail.com>
2025-03-07 20:35:47 -0500

    feat: add Sidebar and SidebarButton component

---
+++ src/components/SidebarButton.jsx
@@ -0,0 +1,16 @@
+const SidebarButton = ({ children, variant }) => {
+  const getVariantClasses = () => {
+    if (variant === 'unselected') {
+      return 'text-[#35383e]';
+    } else if (variant === 'selected') {
+      return 'bg-[#e6f7f8] text-[#00adb5]';
+    }
+  };
+  return (
+    <a href="#" className={`rounded-lg px-6 py-3 ${getVariantClasses()}`}>
+      {children}
+    </a>
+  );
+};
+
+export default SidebarButton;
