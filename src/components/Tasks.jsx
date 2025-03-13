232197bc7b630ffc8ab114d583ba5fd443ff4bd2
Filipe de Araujo <filipearrudafaga97@gmail.com>
2025-03-13 00:20:40 -0400

    feat: ensure task is deleted from db

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -38,7 +38,21 @@ function Tasks() {
   const afternoonTasks = tasks.filter((task) => task.time === 'afternoon');
   const eveningTasks = tasks.filter((task) => task.time === 'evening');
 
-  const handleTaskDeleteClick = (taskId) => {
+  const handleTaskDeleteClick = async (taskId) => {
+    // chamar a API para deletar a tarefa
+
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      return toast.error(
+        'Erro ao deletar a tarefa. Por favor, tente novamente'
+      );
+    }
+
+    // Após chamar a api, vou atualizar o state!
+
     const newTask = tasks.filter((task) => task.id !== taskId);
     setTasks(newTask);
     toast.success('Tarefa deletada com sucesso!');
